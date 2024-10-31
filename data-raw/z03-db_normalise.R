31343b235667b432af70586714ec6183035d3a65
Long <hoanglong.nguyen@outlook.de>
2024-10-31 01:32:03 +0100

    Update database and export to parquet files

--- data-raw/z03-db_normalise.R
+++ data-raw/z03-db_normalise.R
@@ -1,61 +1,42 @@
-source(here::here("data-raw/z00-setup.R"))
+source("data-raw/z00-setup.R")
 
-inkar_raw_duck <- tbl(con, "inkar_raw")
+inkar_raw_duck <- tbl(con, "inkar_raw") |>
+  rename_with(make_clean_names_de)
 
+# The idea is to split the data into one table per Bereich and
+# use the newly available Kürzel as column names.
+# Kürzel are distinct per Bereich, except for some rows in "Zentrale Orte
+# Monitoring" where Kürzel is missing:
+# inkar_raw_duck |>
+#   count(bereich, kuerzel, raumbezug, kennziffer, zeitbezug) |>
+#   filter(n > 1)
+# Otherwise, no discrepancy between data and ref table:
+# inkar_raw_duck |>
+#   anti_join(x = tbl(con, "_indikatoren"), by = join_by(kuerzel)) |>
+#   distinct(bereich, kuerzel)
 
-# Remove columns from raw table -------------------------------------------
+# Join to fill in missing Kürzel
+dbExecute(con, "
+  UPDATE inkar_raw
+  SET Kuerzel = _indikatoren.kuerzel
+  FROM _indikatoren
+  WHERE inkar_raw.id = _indikatoren.merk_id
+    AND inkar_raw.Kuerzel = 'NA';
+")
 
-regionen_duck <- tbl(con, "_regionen")
-
-inkar_values_query <- inkar_raw_duck |>
-  rename_with(make_clean_names_de) |>
-  mutate(
-    kennziffer = as.character(kennziffer),
-    kennziffer = coalesce(kennziffer, kennziffer_eu)
-  ) |>
-  select(indikator_id = id, bereich, raumbezug, kennziffer, zeitbezug, wert) |>
-  left_join(
-    regionen_duck |>
-      mutate(
-        kennziffer = as.character(kennziffer),
-        kennziffer = coalesce(kennziffer, kennziffer_eu)
-      ),
-    by = c("raumbezug", "kennziffer")
-  ) |>
-  select(-c(raumbezug, kennziffer, kennziffer_eu, name)) |>
-  rename(region_id = id) |>
-  relocate(region_id) |>
-  dbplyr::remote_query()
-
-dbExecute(con, paste0("CREATE TABLE inkar_values AS ", inkar_values_query))
-
-inkar_values_duck <- tbl(con, "inkar_values")
-
-
-# Split value table by Bereich --------------------------------------------
-
-# TODO: Foreign key on region_id when supported https://github.com/duckdb/duckdb/issues/46
-
-bereiche <- inkar_values_duck |>
+bereiche <- inkar_raw_duck |>
   distinct(bereich) |>
-  pull() |>
-  setdiff(dbListTables(con))
+  pull()
 
-walk(bereiche, function(bereich) {
-  tbl_subset <- inkar_values_duck |>
+walk(bereiche, \(bereich) {
+  tbl_subset <- inkar_raw_duck |>
     filter(bereich == !!bereich) |>
     select(-bereich)
 
-  varnames <- tbl_subset |>
-    distinct(indikator_id) |>
-    pull() |>
-    sort() |>
-    . => paste0("x", .)
-
   bereich_tidy_query <- tbl_subset |>
-    pivot_wider(names_from = indikator_id, names_prefix = "x", values_from = "wert") |>
-    relocate(all_of(varnames), .after = last_col()) |>
-    arrange(region_id, zeitbezug) |>
+    select(kuerzel, raumbezug, kennziffer, name, zeitbezug, wert) |>
+    pivot_wider(names_from = kuerzel, values_from = wert) |>
+    arrange(raumbezug, kennziffer, zeitbezug) |>
     dbplyr::remote_query()
 
   tbl_name <- make_clean_names_de(bereich)
--- data-raw/z04-db_export.R
+++ data-raw/z04-db_export.R
@@ -1,26 +1,24 @@
-source(here::here("data-raw/z00-setup.R"))
+source("data-raw/z00-setup.R")
 
 dbRemoveTable(con, "inkar_raw")
-dbRemoveTable(con, "inkar_values")
 
 dbListTables(con) |>
   set_names() |>
   map(~ tbl(con, .x))
 
-dbExecute(con, paste0("EXPORT DATABASE '", here::here("inst/db"), "' (FORMAT PARQUET)"))
+dbExecute(con, "EXPORT DATABASE 'inst/db' (FORMAT PARQUET)")
 
 dbDisconnect(con, shutdown = TRUE)
 
-file.remove(here::here("data-raw/inkar.duckdb"))
-file.remove(here::here("data-raw/inkar.duckdb.wal"))
-file.remove(here::here("data-raw/inkar_2021.csv"))
-file.remove(here::here("data-raw/inkar_2021.zip"))
+unlink("data-raw/inkar.duckdb")
+unlink("data-raw/inkar_2024.zip")
+unlink("data-raw/inkar_2024", recursive = TRUE)
 
-read_lines(here::here("inst/db/schema.sql")) |>
+read_lines("inst/db/schema.sql") |>
   keep(startsWith, "CREATE TABLE") |>
-  write_lines(here::here("inst/db/schema.sql"))
+  write_lines("inst/db/schema.sql")
 
-read_lines(here::here("inst/db/load.sql")) |>
+read_lines("inst/db/load.sql") |>
   keep(startsWith, "COPY") |>
   str_replace("FROM '.+/db/", "FROM '$INST_PATH/db/") |>
-  write_lines(here::here("inst/db/load.sql"))
+  write_lines("inst/db/load.sql")
