2825eea845b6418df786243d60cfa8fb48c77a27
Long <hoanglong.nguyen@outlook.de>
2024-10-31 02:19:24 +0100

    Update documentation

--- R/data.R
+++ R/data.R
@@ -2,16 +2,17 @@
 #'
 #' Tables in the INKAR database
 #'
-#' @format A list containing connections to the tables in the local INKAR database.
+#' @format A list containing connections to the tables in the local INKAR
+#'   database.
 #' \describe{
-#'   \item{`_indikatoren`}{ID (official INKAR ID), name, domain and
-#'     description of individual indicators.}
-#'   \item{`_regionen`}{ID (internal ID in the local database),
-#'     spatial reference, official code and name of individual regions.}
+#'   \item{`_indikatoren`}{ID, abbreviation, name, domain and description of
+#'     individual indicators.}
+#'   \item{`_regionen`}{spatial reference, official code and name of individual
+#'     regions.}
 #'   \item{other tables, e.g. `absolutzahlen`}{Values by region and
 #'     time. Each table contains values for a certain INKAR domain (Bereich).
-#'     `region_id` corresponds to the `id` column in the `_regionen` table.
-#'     Column names correspond to the `id` column in the `_indikatoren` table.}
+#'     Column names correspond to the `kuerzel` column in the `_indikatoren`
+#'     table.}
 #' }
 #' The DBI connection to the local database is stored as an attribute of the
 #'   `inkar` object and can be accessed with `attr(inkar, "con")`.
@@ -19,5 +20,6 @@
 
 #' NUTS-3 regions (districts/_Kreise_)
 #'
-#' Lookup table with NUTS-3 code and _Kreiskennziffer_ for all 401 NUTS-3 regions in Germany
+#' Lookup table with NUTS-3 code and _Kreiskennziffer_ for all 401 NUTS-3
+#'   regions in Germany
 "kreise"
